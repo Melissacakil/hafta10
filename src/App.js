3126b56036212cbf24826e022741f2019f5a8729
Melisa Çakıl <melisacakil@gmail.com>
2023-07-09 16:37:22 +0300

    Memo örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Todos from "./Todos";
+import { useState } from "react";
+import React from "react";
+
 function App() {
-  return <p>Merhaba</p>;
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Sayaç: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
