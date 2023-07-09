a0ba403e330265208f5fb545fc2fd33f67ccccd9
Melisa Çakıl <melisacakil@gmail.com>
2023-07-09 17:37:48 +0300

    Prop drilling ile komponentler arası veri transferi yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import AltKomponent from "./AltKomponent";
+
 function App() {
-  return <p>Merhaba</p>;
+  const [sayac, sayacGuncelle] = useState(0);
+
+  return (
+    <>
+      <p>Ben Ana komponentim</p>
+      <button
+        onClick={() => {
+          sayacGuncelle((eski) => eski + 1);
+        }}
+      >
+        +
+      </button>
+
+      <AltKomponent bilgi={sayac} />
+    </>
+  );
 }
 
 export default App;
